eaadd62fb8dd0e7f52e63a5668cf3b6be79de2db
b0songsong <144869765+b0songsong@users.noreply.github.com>
2023-10-30 02:23:27 +0900

    Create 11_example_1.ino

---
+++ 11_example_1.ino
@@ -0,0 +1,13 @@
+#include <Servo.h>
+
+// Arduino pin assignment
+#define PIN_LED   9   // LED active-low
+#define PIN_TRIG  12  // sonar sensor TRIGGER
+#define PIN_ECHO  13  // sonar sensor ECHO
+#define PIN_SERVO 10  // servi motor
+
+// configurable parameters for sonar
+#define SND_VEL 346.0     // sound velocity at 24 celsius degree (unit: m/sec)
+#define INTERVAL 25      // sampling interval (unit: msec)
+#define PULSE_DURATION 10 // ultra-sound Pulse Duration (unit: usec)
+#define _DIST_MIN 180.0   // minimum distan
